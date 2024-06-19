482a808d3cd5a0fcdc2965dda560debe4ff1c7cd
Harshit <rajputharshit48@gmail.com>
2024-06-19 17:04:49 +0530

    responsive edit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { Button } from '@mui/material';
-import React, { useState } from 'react';
-import styles from './navbar.module.css';
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+import styles from "./navbar.module.css";
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -14,28 +14,23 @@ function Navbar() {
   return (
     <div className={styles.navbar}>
       <h1 className={styles.brand}>DAMAC</h1>
-      <Button className={styles['enquires-button']}>
+      <Button className={styles["enquires-button"]}>
         <h1>Enquires</h1>
       </Button>
-      <div className={styles['hamburger-menu']} onClick={toggleMenu}>
+      <div className={styles["hamburger-menu"]} onClick={toggleMenu}>
         ☰
       </div>
       {menuOpen && (
-        <div className={styles['options-menu']}>
-    {     [1,2,3,4].map((i) => (
-          <div className='w-full flex flex-col'>
-          <a href="#" className={`${styles["optionAnchor"]}`}>Option {i}</a>
-          <div className='h-[1px] w-[145px] ml-[-10px] bg-white'></div>
-          </div>
-         
-         )
-         
-        )
-      
-      }
-
-      
-         </div>
+        <div className={styles["options-menu"]}>
+          {[1, 2, 3, 4].map((i) => (
+            <div className="w-full flex flex-col">
+              <a href="#" className={`${styles["optionAnchor"]}`}>
+                Option {i}
+              </a>
+              <div className="h-[1px] w-[145px] ml-[-10px] bg-white"></div>
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
